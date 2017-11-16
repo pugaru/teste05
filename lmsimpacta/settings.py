604a33b6a1bd6e1f0c409375a3b3a34f4d6fd41b
pugaru <30847672+pugaru@users.noreply.github.com>
2017-11-16 12:02:43 -0200

    Update settings.py

--- lmsimpacta/settings.py
+++ lmsimpacta/settings.py
@@ -152,5 +152,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+STATIC_URL = "/home/site/wwwroot/core/static/"
+STATIC_ROOT = ""
